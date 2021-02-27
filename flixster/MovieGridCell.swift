ddefdc79b7ea5f44d9e21acb4bf4e783171e747a
AjazSyed <aks3872@uw.edu>
2021-02-26 23:34:39 -0800

    Added collectionView and moviesDetails

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Ajaz Syed on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -67,6 +67,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
         
     }
+    
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row ]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+         
+        
+    }
 
 
 }
